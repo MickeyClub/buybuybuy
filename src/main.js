97867a7805fda3459cffa56583003b2e42e86f27
MickeyClub <972207518@qq.com>
2018-10-09 16:46:43 +0800

    完成首页搭建

--- src/main.js
+++ src/main.js
@@ -3,6 +3,34 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入路由
+import VueRouter from 'vue-router'
+// 导入首页组件
+import index from './components/index.vue'
+
+// 一定要use一下
+Vue.use(VueRouter)
+
+// 路由规则
+const routes = [
+  {
+    path: '/',
+    component: index
+  },
+  {
+    path: '/index',
+    component: index
+  }
+]
+
+// 实例化路由
+const router = new VueRouter({
+  routes
+})
+
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // 挂在路由
+  router
 }).$mount('#app')
